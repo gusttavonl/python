9d329779902e7959c0cd1a011f843363748ba40f
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-10-24 15:25:15 -0300

    feat: add math operators example

---
+++ basic/math-operators.py
@@ -0,0 +1,20 @@
+num1 = 10
+num2 = 10
+
+sum = num1 + num2
+print(sum)
+
+sub = num1 - num2
+print(sub)
+
+div = num1 / num2
+print(div)
+
+mult = num1 * num2
+print(mult)
+
+elev = num1 ** num2
+print(elev)
+
+float = 10.02
+print(int(float))
