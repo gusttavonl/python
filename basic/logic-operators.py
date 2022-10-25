dbc1b5a83cfdae545b96be945ed96e9c627bdf20
GustavoNoronha0 <admin@gustavonoronha.com.br>
2022-10-25 13:27:39 -0300

    feat: add logic operators code example

---
+++ basic/logic-operators.py
@@ -0,0 +1,17 @@
+# the result is true or false
+thisLanguageIsPython = True
+print(thisLanguageIsPython)
+
+thisLanguageIsLowCode = True
+
+print(thisLanguageIsPython and thisLanguageIsLowCode)
+print(thisLanguageIsPython or thisLanguageIsLowCode)
+
+num1 = 10
+num2 = 10
+print(num1 == num2)
+print(num1 != num2)
+print(num1 < num2)
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 >= num2)
